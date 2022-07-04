c14aaeac3a81c19c6f8fd8a674f43b29b60cc202
carlos97gr <carlosgperu97@gmail.com>
2022-07-04 18:30:46 +0300

    adding firebase logo and color for social logos

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -7,8 +7,7 @@ import {
 } from "./index.style";
 import { FaTwitter, FaInstagram, FaYoutube, FaLinkedin } from "react-icons/fa";
 
-const size = 21;
-const color = "#000000";
+const size = 25;
 
 export const Navbar = () => {
   return (
@@ -25,28 +24,28 @@ export const Navbar = () => {
           target="_blank"
           rel="noreferrer"
         >
-          <FaTwitter size={size} color={color} />
+          <FaTwitter size={size} color={"#1DA1F2"} />
         </SocialIcon>
         <SocialIcon
           href="https://www.linkedin.com/in/carlos97gr/"
           target="_blank"
           rel="noreferrer"
         >
-          <FaLinkedin size={size} color={color} />
+          <FaLinkedin size={size} color={"#0077B5"} />
         </SocialIcon>
         <SocialIcon
           href="https://www.instagram.com/carlos97cgr/"
           target="_blank"
           rel="noreferrer"
         >
-          <FaInstagram size={size} color={color} />
+          <FaInstagram size={size} color={" #833AB4"} />
         </SocialIcon>
         <SocialIcon
           href="https://www.youtube.com/channel/UC-xrCcsYNYMs4iIvAja0Gig"
           target="_blank"
           rel="noreferrer"
         >
-          <FaYoutube size={size} color={color} />
+          <FaYoutube size={size} color={"#FF0000"} />
         </SocialIcon>
       </div>
     </NavbarContainer>
