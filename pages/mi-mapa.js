da114ee0565460c9b11057f9f1c7d0c2a8f2b21b
carlos97gr <carlosgperu97@gmail.com>
2022-08-08 15:01:50 +0400

    changing size of map

--- pages/mi-mapa.js
+++ pages/mi-mapa.js
@@ -2,7 +2,7 @@ export default function MiMapa() {
   return (
     <iframe
       width="100%"
-      height="100%"
+      height="750px"
       src="https://datastudio.google.com/embed/reporting/5acad0ed-5914-42e2-a02a-25f3da3dcc31/page/6QgzC"
       frameBorder="0"
       style={{border:0}}
