6eede0c2903b3bcc3e031ffaa8fb9fbc442140ea
carlos97gr <carlosgperu97@gmail.com>
2022-08-08 10:24:08 +0400

    adding map

---
+++ pages/mi-mapa.js
@@ -0,0 +1,12 @@
+export default function MiMapa() {
+  return (
+    <iframe
+      width="100%"
+      height="100%"
+      src="https://datastudio.google.com/embed/reporting/5acad0ed-5914-42e2-a02a-25f3da3dcc31/page/6QgzC"
+      frameBorder="0"
+      style={{border:0}}
+      allowFullScreen
+    ></iframe>
+  );
+}
