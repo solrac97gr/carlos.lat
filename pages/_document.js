4a5580e01d00a44990e2d1595040bf20d59265bf
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-04-03 16:20:28 -0500

    add google adsense code

--- pages/_document.js
+++ pages/_document.js
@@ -1,7 +1,6 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
-import { ServerStyleSheet } from 'styled-components'
-
+import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
@@ -34,6 +33,11 @@ class MyDocument extends Document {
     return (
       <Html lang="es">
         <Head>
+          <script
+            async
+            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9432125869132148"
+            crossOrigin="anonymous"
+          ></script>
           <Script
             id="google-tag-manager"
             strategy="afterInteractive"
