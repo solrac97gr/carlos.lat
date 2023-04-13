1ba51bac66c8b6b7f148f579358b451fbf4306a8
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-04-13 16:24:55 -0500

    fix style

--- components/Bio/index.styles.js
+++ components/Bio/index.styles.js
@@ -3,7 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  padding: 20%;
+  padding: 15%;
   @media (max-width: 768px) {
     padding: 20% 5% 20% 5%;
   }
@@ -16,5 +16,4 @@ export const LinkStyled = styled.a`
   cursor: pointer;
 `;
 
-export const Content = styled.p`
-`;
+export const Content = styled.p``;
