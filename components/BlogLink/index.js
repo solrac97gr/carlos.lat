cd9ec318348d2d73acdaf44b4aaecc618eab8063
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-04-19 15:53:35 -0500

    blog visit event

--- components/BlogLink/index.js
+++ components/BlogLink/index.js
@@ -1,12 +1,18 @@
-export const BlogLink = ({ url, content }) => {
-  return (
-    <a
-      href={url}
-      target="_blank"
-      rel="noreferrer"
-      style={{ color: "#63f3ab", textDecoration: "underline" }}
-    >
-      {content?content:url}
-    </a>
-  );
+import {logEvent} from "../../lib/analytics";
+
+export const BlogLink = ({url, content}) => {
+    return (
+        <a
+            onClick={() => {
+              logEvent("Button", "Click_Blog", content ? content : url, url);
+              }
+            }
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            style={{color: "#63f3ab", textDecoration: "underline"}}
+        >
+            {content ? content : url}
+        </a>
+    );
 };
