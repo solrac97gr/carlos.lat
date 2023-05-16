d24d6b8cb65c11392f7f536084f74a9582abdeb9
Carlos García Rosales <carlosgarcia_97gr@hotmail.com>
2023-05-16 19:59:19 +0300

    creating page and structure of the courses

---
+++ pages/cursos.js
@@ -0,0 +1,30 @@
+const courses = [
+  {
+    name: "Curso de ASP.NET MVC 5",
+    link: "https://hotm.art/ehFVSzt",
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-18ce8.appspot.com/o/cursos%2FASP.NET%20MVC%205%20Course.png?alt=media&token=37392c13-95dc-4fe8-a6a8-e6d7e9771820",
+    difficulty: "Principiante",
+    description:
+      "En este curso aprenderás todo lo necesario para usar el Patron MVC(Modelo-Vista-Controlador) para programar aplicaciones web ASP.NET MVC 5 usando Visual Studio 2022.Con una estructura sencilla, fácil, práctica y amena, donde podrás potenciar los conocimientos que ya tienes o aprender desde cero. Adicionalmente recibe el acompañamiento necesario para que puedas crear Aplicaciones web ASP.NET MVC 5 para tus emprendimientos o para conseguir trabajo como desarrollador ya que esta es una habilidad de alta demanda en la actualidad",
+    creator: "successforallacademy.com",
+    tag: ["ASP.NET"],
+  },
+  {
+    name: "Curso de Python en Español",
+    link: "https://hotm.art/ehFVSzt",
+    photo:
+      "https://firebasestorage.googleapis.com/v0/b/portfolio-18ce8.appspot.com/o/cursos%2FCreate%20a%20instagram%20post%20for%20my%20feed%20about%20this%20new%20python%20course%20but%20in%20Spanish%20use%20a%20gradient%20in%20the%20background%20and%20center%20the%20image%20try%20to%20make%20it%20the%20most%20minimalist%20as%20you%20can%20and%20add%20a%20call%20to%20action%20in%20Spanish%20also.png?alt=media&token=8cf6653f-8ddc-4d95-932f-098e8d1f0970",
+    difficulty: "Principiante",
+    description:
+      "En este curso aprenderás todo lo que necesitas saber sobre el lenguaje de Python. Luego de terminarlo podrás decidir sobre qué área de la programación involucrarte, ya que se abre un mundo de opciones.",
+    creator: "aprendopromas.com",
+    tag: ["python"],
+  },
+];
+
+export default function SobreMi() {
+  return <div>
+    Cursos
+  </div>;
+}
